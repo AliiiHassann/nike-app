ba182a37ce5a0eecb7766ecdcf59772759862a82
AliiiHassann <alihassan71010@gmail.com>
2024-06-07 22:10:33 +0200

    nav changes

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -5,12 +5,16 @@ import { BsCart } from "react-icons/bs";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import logo from "../assets/Nike/logo.png";
-import {Container} from "react-bootstrap"
+import { Container } from "react-bootstrap";
 import "../style/NavbarItem.css";
-import { selectCartItems, selectTotalQTY, setGetTotals } from "../rtk/slices/PopularSlice";
+import {
+  selectCartItems,
+  selectTotalQTY,
+  setGetTotals,
+} from "../rtk/slices/PopularSlice";
 function NavbarItem() {
-  const totalQTY = useSelector(selectTotalQTY)
-  const dispatch = useDispatch()
+  const totalQTY = useSelector(selectTotalQTY);
+  const dispatch = useDispatch();
   const [navState, setNavState] = useState(false);
   const cartItems = useSelector(selectCartItems);
   const onNavScroll = () => {
@@ -22,7 +26,7 @@ function NavbarItem() {
   };
   useEffect(() => {
     window.addEventListener("scroll", onNavScroll);
-    dispatch(setGetTotals())
+    dispatch(setGetTotals());
   }, [cartItems, dispatch]);
   return (
     <>
@@ -33,41 +37,41 @@ function NavbarItem() {
             : "  d-flex pt-3 pb-3 fixed-top navState align-items-center justify-content-center"
         }
       >
-          <Container>
-          <nav className="d-flex align-items-center justify-content-between m-auto">
-          <div className="img">
-            <img
-              className="logo-photo"
-              src={logo}
-              alt=""
-              style={{ width: "60px" }}
-            />
-          </div>
-          <ul className="list-unstyled d-flex gap-4 mb-0 p-2">
-            <li className="list-unstyled text-white ">
-              <BsSearch style={{ fontSize: "25px" }} />
-            </li>
-            <li className="list-unstyled text-white">
-              <BsSuitHeart style={{ fontSize: "25px" }} />
-            </li>
-            <li className="list-unstyled position-relative">
-              <Link
-                to={"cart"}
-                className="p-0 m-0 border-0 bg-transparent text-white btn"
-              >
-                <BsCart style={{ fontSize: "25px" }} className="cart-icon" />
+        <Container>
+          <nav className='d-flex align-items-center justify-content-between m-auto'>
+            <div className='img'>
+              <img
+                className='logo-photo'
+                src={logo}
+                alt=''
+                style={{ width: "60px" }}
+              />
+            </div>
+            <ul className='list-unstyled d-flex gap-4 mb-0 p-2'>
+              <li className='list-unstyled text-white '>
+                <BsSearch style={{ fontSize: "25px" }} />
+              </li>
+              <li className='list-unstyled text-white'>
+                <BsSuitHeart style={{ fontSize: "25px" }} />
+              </li>
+              <li className='list-unstyled position-relative'>
                 <Link
-                  to={"/cart"}
-                  className="cart-number"
-                  style={{ textDecoration: "none" }}
+                  to={"cart"}
+                  className='p-0 m-0 border-0 bg-transparent text-white btn'
                 >
-                  <div>{totalQTY}</div>
+                  <BsCart style={{ fontSize: "25px" }} className='cart-icon' />
+                  <Link
+                    to={"/cart"}
+                    className='cart-number'
+                    style={{ textDecoration: "none" }}
+                  >
+                    <div>{totalQTY}</div>
+                  </Link>
                 </Link>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-          </Container>
+              </li>
+            </ul>
+          </nav>
+        </Container>
       </header>
     </>
   );
